d2dfd06f36b081bb72cbcf2d69350b2e8225b7ee
pralolik <krotov.evg.mk@gmail.com>
2017-04-12 04:06:51 +0300

    Add Service, Controllers, Form, View

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\RegisterType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+
+
+    /**
+     * @Route("/login", name="user_login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('user/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registerService = $this->get('app.security.register');
+            $registerService->registerUser($user);
+            return $this->redirectToRoute('user_login');
+        }
+        $errors = (string) $form->getErrors(true);
+        return $this->render(
+            'user/register.html.twig',
+            array('form' => $form->createView(),
+                'errors'=>$errors)
+        );
+    }
+
+    /**
+     * @Route("/activate/{token}", name="user_activate")
+     */
+    public function activateAction(string $token)
+    {
+        $activateService = $this->get('app.security.activator');
+        $activateService->activation($token);
+        return $this->redirect($this->generateUrl('user_login'));
+    }
+
+    /**
+     * @Route("/logout", name="user_logout")
+     */
+    public function logoutAction()
+    {
+    }
+
+
+}
